9db05b086d1adb54c711a1b63c5b13b4005cdd60
Marcelo Boeira <me@marceloboeira.com>
2016-03-29 00:08:23 -0300

    feature: Add support for timestamps

--- spec/src/line_spec.cr
+++ spec/src/line_spec.cr
@@ -2,7 +2,13 @@ require "./../spec_helper"
 
 describe SRT::Line do
   context "when rendering" do
-    line = SRT::Line.new(sequence: 7, text: "Hello")
+    starts_at = Time.new(Time::Span.new(0, 4, 32, 2, 32).ticks)
+    finishs_at = Time.new(Time::Span.new(0, 5, 7, 3, 27).ticks)
+
+    line = SRT::Line.new(sequence: 7,
+                         starts_at: starts_at,
+                         finishs_at: finishs_at,
+                         text: "Hello")
 
     it "renders the sequence" do
       line.to_s.should match(/7/)
@@ -12,8 +18,20 @@ describe SRT::Line do
       line.to_s.should match(/Hello/)
     end
 
+    it "renders the starts_at" do
+      line.to_s.should match(/04:32:02,032/)
+    end
+
+    it "renders the finishs_at" do
+      line.to_s.should match(/05:07:03,027/)
+    end
+
+    it "renders the separator" do
+      line.to_s.should match(/-->/)
+    end
+
     it "renders in the correct positions" do
-      line.to_s.should eq("7\nHello")
+      line.to_s.should eq("7\n04:32:02,032 --> 05:07:03,027\nHello")
     end
   end
 end
--- src/srt/line.cr
+++ src/srt/line.cr
@@ -1,12 +1,17 @@
 module SRT
   class Line
+    TIME_FORMAT = "%H:%M:%S,%L"
+    SEPARATOR = " --> "
     property sequence : Int32,
+             starts_at : Time,
+             finishs_at : Time,
              text : String
 
-    def initialize(@sequence = 0, @text = ""); end
+    def initialize(@sequence = 0, @starts_at = Time.now, @finishs_at = Time.now, @text = ""); end
 
     def to_s
       "#{sequence}\n" +
+      "#{starts_at.to_s(TIME_FORMAT)}#{SEPARATOR}#{finishs_at.to_s(TIME_FORMAT)}\n" +
       "#{text}"
     end
   end
