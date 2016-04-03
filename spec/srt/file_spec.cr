a3e39629f89dd26039b59903542e33d80814f6e8
Marcelo Boeira <me@marceloboeira.com>
2016-04-03 13:33:45 -0300

    feature: Parse from file input

--- spec/srt/file_spec.cr
+++ spec/srt/file_spec.cr
@@ -1,12 +1,33 @@
 require "./../spec_helper"
 
 describe SRT::File do
+  let(two_lines) { Fixture.load("two-lines.srt") }
+  let(lines) {
+    [ SRT::Line.new(1, Helper.smart_time(0, 2, 50, 904), Helper.smart_time(0, 2, 52, 929), "Help me!"),
+      SRT::Line.new(2, Helper.smart_time(0, 2, 53, 6), Helper.smart_time(0, 2, 55, 406), "Somebody, help me out.") ]
+  }
+
+  context "when parsing" do
+    context "with valid params" do
+      let(file) { SRT::File.parse(two_lines) }
+
+      it "parses all lines" do
+        expect(file.lines.size).to eq(2)
+
+        expect(file.lines.first.sequence).to eq(1)
+        expect(file.lines.first.text).to eq("Help me!")
+
+        expect(file.lines.last.sequence).to eq(2)
+        expect(file.lines.last.text).to eq("Somebody, help me out.")
+      end
+
+      it "reproduces the same input when rendering" do
+        expect(file.to_s).to eq(two_lines)
+      end
+    end
+  end
+
   context "when rendering" do
-    let(two_lines) { Fixture.load("two-lines.srt") }
-    let(lines) {
-      [ SRT::Line.new(1, Helper.smart_time(0, 2, 50, 904), Helper.smart_time(0, 2, 52, 929), "Help me!"), 
-        SRT::Line.new(2, Helper.smart_time(0, 2, 53, 6), Helper.smart_time(0, 2, 55, 406), "Somebody, help me out.") ]
-    }
     let(file) { SRT::File.new(lines: lines) }
 
     it "render the lines in sequence" do
--- src/srt/file.cr
+++ src/srt/file.cr
@@ -13,5 +13,23 @@ module SRT
 
       buffer
     end
+
+    def self.parse(input : String)
+      input_lines = input.lines
+      lines = [] of Line
+      buffer = ""
+
+      input.lines.each do |line|
+        buffer += line
+
+        if line.strip.empty?
+          lines << Line.parse(buffer)
+
+          buffer = ""
+        end
+      end
+
+      new(lines: lines)
+    end
   end
 end
